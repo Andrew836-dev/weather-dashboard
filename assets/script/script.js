ce358f11896eda15516fbbed4a1cfd7384680a1b
Andrew Gray <andrew.aj.gray@gmail.com>
2020-06-21 13:14:14 +0930

    Script calls several APIs and formats all responses to (mostly) readable data

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,51 +1,77 @@
 $("document").ready(function () {
-    var lastWeather;
+    var lastWeather = localStorage.getItem("lastWeather");
+    var lastForecast = localStorage.getItem("lastForecast");
     var lastCity = localStorage.getItem("lastCity");
-    var units = "metric";
+    var uvTag = $("#uv-tag");
+    var displayUnits = "metric";
+    var today = moment().format("X");
+    console.log(today);
+    // var units = {
+    //     "metric": {
+    //         temp = "C",
+    //         wind = "m/s"
+    //     }
+    // }
 
-    function getWeather(searchCity) {
-        if (searchCity) {
-            var queryUrl = "https://api.openweathermap.org/data/2.5/weather?appid=fc2d1f9e61e29d3aff30681d900d7f1b&q=" + searchCity + "&units=" + units;
-            $.ajax({
-                url: queryUrl,
-                method: "GET"
-            }).then(function (response) {
-                displayWeather(response);
-            });
-        }
+    function getUV(lat, lon) {
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            beforeSend: function (request) {
+                $("#uv-tag").html("retrieving UV-index");
+                request.setRequestHeader('x-access-token', '9cc70a7bbb61df3f008fa358b3bb0489');
+            },
+            url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lon
+        }).then(function (response) {
+            $("#uv-tag").html(response.result.uv);
+        });
     }
 
-    function getForecast(searchCity) {
-        var queryUrl = "https://api.openweathermap.org/data/2.5/weather?appid=fc2d1f9e61e29d3aff30681d900d7f1b&q=" + searchCity;
+    function callAPI(type, searchCity) {
+        var queryUrl = "https://api.openweathermap.org/data/2.5/" + type + "?appid=fc2d1f9e61e29d3aff30681d900d7f1b&q=" + searchCity + "&units=" + displayUnits;
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            displayForecast(response);
+            if (type == "weather") {
+                displayWeather(response);
+            }
+            else if (type == "forecast") {
+                displayForecast(response);
+            }
         });
     }
 
     function displayWeather(weatherData) {
-        var name = weatherData.name;
-        var date
-        var weathericon
-        var temperature = weatherData.main.temp;
-        var humidity
-        var windSpeed
-        var uvIndex
+        console.log("Latest Data");
+        console.log(weatherData);
         $("#current-weather").empty();
-        $("#current-weather").append($("<div>").text(name + " " + temperature))
+        $("#current-weather").append(
+            $("<h1>").text(weatherData.name + "(" + moment().format("M/D/YYYY") + ") Icon: " + weatherData.weather[0].icon),
+            $("<p>").text("Temperature: " + weatherData.main.temp),
+            $("<p>").text("Humidity: " + weatherData.main.humidity),
+            $("<p>").text("Wind speed: " + weatherData.wind.speed),
+            uvTag
+        );
+        getUV(weatherData.coord.lat, weatherData.coord.lon);
+        addCity(weatherData.name);
     }
 
     function displayForecast(forecastData) {
-        var date
-        var weathericon
-        var temperature
-        var humidity
+        $("#forecast-weather").empty();
+        for (var i = 7; i < 40; i += 7){
+            $("#forecast-weather").append($("<div>").append(
+                $("<h3>").text(moment(forecastData.list[i].dt, "X").format("M/D/YYYY")),
+                $("<p>").text("Icon: " + forecastData.list[i].weather[0].icon + forecastData.list[i].weather[0].description + forecastData.list[i].weather[0].main),
+                $("<p>").text("Temp: " + forecastData.list[i].main.temp),
+                $("<p>").text("Humidity: " + forecastData.list[i].main.humidity)
+            ));
+        }
+        console.log("Forecast Data");
         console.log(forecastData);
     }
 
-    // removes duplicates and keeps most recent search at the top and in localStorage
+    // removes duplicates and keeps most recent successful search at the top and in localStorage
     function addCity(cityName) {
         $("#search-history").children().each(function () {
             if ($(this).text().toUpperCase() == cityName.toUpperCase()) {
@@ -56,14 +82,21 @@ $("document").ready(function () {
         localStorage.setItem("lastCity", cityName);
     }
 
-    getWeather(lastCity);
+    function init() {
+        if (lastCity) {
+            callAPI("weather", lastCity);
+            callAPI("forecast", lastCity);
+        }
+    }
+
+    init();
     $("#search-btn").on("click", function (event) {
         event.preventDefault();
         var searchCity = $("#search-text").val().trim();
         if (searchCity) {
-            getWeather(searchCity);
+            callAPI("weather", searchCity);
+            callAPI("forecast", searchCity);
             // getForecast(searchCity);
-            addCity(searchCity);
         }
     })
 });
